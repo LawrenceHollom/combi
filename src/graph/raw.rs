a5a2a3fe672741c1be00b6ed61bf20f1ab8beb59
Lawrence <lawrence@hollom.com>
2023-01-19 18:50:36 +0000

    Implement dodecahedron

--- src/graph/raw.rs
+++ src/graph/raw.rs
@@ -54,7 +54,27 @@ pub fn new_icosahedron() -> Graph {
 }
 
 pub fn new_dodecahedron() -> Graph {
-    let adj_list: Vec<Vec<usize>> = vec![vec![1, 4, 5], vec![0, 2, 6], vec![1, 3, 7], vec![2, 4, 8], vec![0, 3, 9],
-        vec![0, 7, 8], vec![1, 8, 9], vec![2, 5, 9], vec![3, 5, 6], vec![4, 6, 7]];
+    let adj_list: Vec<Vec<usize>> = vec![
+        vec![2,3,4],
+        vec![5,6,7],
+        vec![0,8,9],
+        vec![0,10,12],
+        vec![0,11,13],
+        vec![1,14,15],
+        vec![1,16,18],
+        vec![1,17,19],
+        vec![2,10,17],
+        vec![2,11,16],
+        vec![3,8,19],
+        vec![4,9,18],
+        vec![3,13,14],
+        vec![4,12,15],
+        vec![5,12,19],
+        vec![5,13,18],
+        vec![6,9,17],
+        vec![7,8,16],
+        vec![6,11,15],
+        vec![7,10,14],
+    ];
     of_adj_list(adj_list, Dodecahedron)
 }
