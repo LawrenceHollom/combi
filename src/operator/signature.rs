6a3cbb69c6ae7639b53a8bb54c04f88290366a51
LawrenceHollom <lawrence@hollom.com>
2023-02-27 10:43:24 -0300

    singature

--- src/operator/signature.rs
+++ src/operator/signature.rs
@@ -1,7 +1,27 @@
-use crate::graph::*;
-
-use super::domination::*;
+use crate::{graph::*, operator::Operator};
 
 pub fn print_signature(g: &Graph) {
-    println!("gamma: {}", domination_number(g));
+    let mut properties: Vec<(String, String)> = vec![];
+    use crate::operation::int_operation::IntOperation::*;
+    let int_operations = 
+        vec![Order, Size, NumComponents, MinDegree, MaxDegree, 
+        DominationNumber, ChromaticNumber, Girth, Connectedness];
+    use crate::operation::bool_operation::BoolOperation::*;
+    let bool_operations =
+        vec![IsPlanar, IsTriangleFree];
+    let mut operator = Operator::new();
+    for op in int_operations.iter() {
+        properties.push((format!("{}", op), format!("{}", operator.operate_int(g, op))));
+    }
+    for op in bool_operations.iter() {
+        properties.push((format!("{}", op), format!("{}", operator.operate_bool(g, op))));
+    }
+    let max_len = properties.iter().map(|(s, _)| s.len()).max().unwrap();
+    for (s1, s2) in properties.iter() {
+        let pad_len = max_len - s1.len();
+        for _i in 0..(pad_len + 1) {
+            print!(" ");
+        }
+        println!("{} : {}", *s1, *s2);
+    }
 }
