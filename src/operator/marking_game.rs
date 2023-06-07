6dd52ec1e60d8be85cae3f327ad4fa50214a464b
Lawrence <lawrence@hollom.com>
2023-06-07 18:51:24 +0100

    Fixed marking_game

--- src/operator/marking_game.rs
+++ src/operator/marking_game.rs
@@ -5,18 +5,16 @@ use crate::graph::*;
 use utilities::vertex_tools::*;
 
 fn marking_game_number_rec(g: &Graph, marked: VertexSet, num_marked: usize, 
-        history: &mut HashMap<VertexSet, usize>, current_score: usize, min_final_score: usize) -> usize {
+        history: &mut HashMap<VertexSet, usize>) -> usize {
     if g.n.at_most(num_marked) {
-        current_score
+        0
     } else {
         match history.get(&marked) {
             Some(score) => *score,
             None => {
                 let is_maker_turn = num_marked % 2 == 0;
-                let mut running_score = current_score;
-                let mut final_score = if is_maker_turn { usize::MAX } else { 0 };
-                let mut running_min_final_score = min_final_score;
-                'test_verts: for v in g.iter_verts() {
+                let mut best_remaining_score = if is_maker_turn { usize::MAX } else { 0 };
+                for v in g.iter_verts() {
                     if !marked.has_vert(v) {
                         //try marking u.
                         let mut num_adj_marked = 0;
@@ -25,28 +23,19 @@ fn marking_game_number_rec(g: &Graph, marked: VertexSet, num_marked: usize,
                                 num_adj_marked += 1;
                             }
                         }
-                        // We have to mark it at some point.
-                        running_score = running_score.max(num_adj_marked);
-                        if is_maker_turn && num_adj_marked > running_min_final_score {
-                            // maker can give up as better has been done.
-                            break 'test_verts;
-                        } else {
-                            let new_marked = marked.add_vert_immutable(v);
-                            let resulting_score = marking_game_number_rec(g, new_marked, num_marked + 1,
-                                history, running_score, running_min_final_score);
-                            if is_maker_turn && resulting_score < running_min_final_score {
-                                running_min_final_score = resulting_score;
-                            }
-                            if is_maker_turn && resulting_score < final_score {
-                                final_score = resulting_score;
-                            } else if !is_maker_turn && resulting_score > final_score {
-                                final_score = resulting_score;
-                            }
+                        let new_marked = marked.add_vert_immutable(v);
+                        let remaining_score = marking_game_number_rec(g, new_marked, num_marked + 1,
+                            history).max(num_adj_marked);
+                        if is_maker_turn && remaining_score < best_remaining_score {
+                            best_remaining_score = remaining_score;
+                        } else if !is_maker_turn && remaining_score > best_remaining_score {
+                            best_remaining_score = remaining_score;
                         }
                     }
                 }
-                history.insert(marked, final_score);
-                final_score
+                marked.print();
+                history.insert(marked, best_remaining_score);
+                best_remaining_score
             }
         }
     }
@@ -54,6 +43,5 @@ fn marking_game_number_rec(g: &Graph, marked: VertexSet, num_marked: usize,
 
 pub fn marking_game_number(g: &Graph) -> u32 {
     let mut history = HashMap::new();
-    (marking_game_number_rec(g, VertexSet::new(g.n), 0, &mut history, 
-        0, usize::MAX) + 1) as u32
+    (marking_game_number_rec(g, VertexSet::new(g.n), 0, &mut history) + 1) as u32
 }
