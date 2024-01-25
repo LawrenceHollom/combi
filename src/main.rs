39b440c8b56ac375064ab74e4d9013c01f62bfa1
Lawrence <lawrence@hollom.com>
2024-01-25 19:10:44 +0000

    Accept command-line input

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,27 @@ mod digraph;
 
 use controller::*;
 
+fn execute_instruction(text: &String) {
+    let start_time = SystemTime::now();
+    let instruction = Instruction::of_string(text);
+    println!("Instruction constructed!\n{},\nTime: {}", instruction, start_time.elapsed().unwrap().as_millis());
+
+    instruction.execute();
+    println!("Finished! Time (s): {}", start_time.elapsed().unwrap().as_secs())
+}
+
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    loop {
-        println!("Enter instruction:");
-        let mut text = String::new();
-        io::stdin().read_line(&mut text).expect("Failed to read line");
-        let start_time = SystemTime::now();
-        let instruction = Instruction::of_string(&text);
-        println!("Instruction constructed!\n{},\nTime: {}", instruction, start_time.elapsed().unwrap().as_millis());
-
-        instruction.execute();
-        println!("Finished! Time (s): {}", start_time.elapsed().unwrap().as_secs())
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        println!("Argument received, running directly!");
+        execute_instruction(&args[1])
+    } else {
+        loop {
+            println!("Enter instruction:");
+            let mut text = String::new();
+            io::stdin().read_line(&mut text).expect("Failed to read line");
+            execute_instruction(&text)
+        }
     }
 }
