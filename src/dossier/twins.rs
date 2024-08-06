25cdecd3f56cd5ca024ce46bfa67360314fb605c
LawrenceHollom <lawrence@hollom.com>
2024-08-06 20:30:00 +0100

    Tiny bit more twints

--- src/dossier/twins.rs
+++ src/dossier/twins.rs
@@ -1,15 +1,29 @@
 use crate::entity::*;
 use crate::entity::poset::*;
 
+/**
+ * Elements are twin elements if they cannot be distinguished by their
+ * neighbours; e.g. they cover and are covered by the same elements,
+ * or have the same adj, or similar.
+ */
+
 fn poset_has_twin_elements(p: &Poset) -> bool {
+
+}
+
+fn graph_has_twin_elements(g: &Graph) -> bool {
+
+}
+
+fn digraph_has_twin_elements(g: &Digraph) -> bool {
 
 }
 
 pub fn has_twin_elements(e: &Entity) -> bool {
     use Entity::*;
     match e {
-        Graph(_) => panic!("You haven't coded this yet!"),
-        Digraph(_) => panic!("You haven't coded this yet!"),
+        Graph(g) => graph_has_twin_elements(g),
+        Digraph(d) => digraph_has_twin_elements(d),
         Poset(p) => poset_has_twin_elements(p),
     }
 }
