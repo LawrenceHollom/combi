52ab4d50f78b89990780fde9d51ce88f262399e9
Lawrence <lawrence@hollom.com>
2024-09-10 19:02:02 +0100

    Remove debug logging

--- src/dossier/kernels.rs
+++ src/dossier/kernels.rs
@@ -26,20 +26,16 @@ pub fn min_kernel_size(d: &Digraph, print_min: bool) -> u32 {
             let inter = out_nbrs[v].inter(&s);
             if inter.is_nonempty() {
                 // s isn't a stable set.
-                // print!("Not stable!"); s.print_hum();
                 let w = inter.get_biggest_element().unwrap();
                 // v and w can't both be present, so we can increment s to avoid this conflict.
                 s.incr_inplace_to_remove_vertex(v.min(w));
                 continue 'iter_subsets
-            } else {
-                // print!("Stable! ");
             }
         }
 
         let size = s.size();
         if size >= min_kernel_size {
             // Don't bother with big things.
-            // print!("Too big!"); s.print_hum();
             s.incr_inplace();
             continue 'iter_subsets
         }
@@ -53,18 +49,16 @@ pub fn min_kernel_size(d: &Digraph, print_min: bool) -> u32 {
 
         if covered_verts.is_everything() {
             // This is a kernel.
-            // print!("Just right!");
             min_kernel_size = size;
             optimal_set = s;
         }
-        // s.print_hum();
         s.incr_inplace();
     }
 
-    // if min_kernel_size != new_min_kernel_size {
-    //     d.print();
-    //     panic!("Old and new calculations disagree!")
-    // }
+    if print_min {
+        print!("Minimal 2-kernel: ");
+        optimal_set.print_hum();
+    }
 
     min_kernel_size as u32
 }
