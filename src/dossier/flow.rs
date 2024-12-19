cddc3ff34502ec7d1598aa712fae96b7acdae7e1
Lawrence <lawrence@hollom.com>
2024-12-19 19:47:58 +0000

    Add flow.rs

---
+++ src/dossier/flow.rs
@@ -0,0 +1,28 @@
+use crate::entity::digraph::*;
+
+/**
+ * This is for flows on digraphs.
+ */
+
+/**
+ * Returns true if there is some vertex v such that, for every x in D,
+ * there is a walk from x to v.
+ * Uses Floyd--Warshall, and so runs in n^3 time.
+ */
+pub fn has_global_basin(d: &Digraph) -> bool {
+    let dist = d.floyd_warshall();
+    for v in d.iter_verts() {
+        let mut is_v_basin = true;
+        // Can we flow from anywhere to v?
+        'test_v: for u in d.iter_verts() {
+            if u != v && dist[u][v].is_none() {
+                is_v_basin = false;
+                break 'test_v;
+            }
+        }
+        if is_v_basin {
+            return true
+        }
+    }
+    false
+}
